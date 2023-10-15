d80ae596f679619493f146d0ef7de1190e338c6e
Romchik <romankolesov44@gmail.com>
2023-10-16 00:22:35 +0500

    Update SELECT_Roman_Kolesov.sql

--- SELECT_Roman_Kolesov.sql
+++ SELECT_Roman_Kolesov.sql
@@ -44,5 +44,31 @@ WHERE
 
 -- Which five movies were rented more than the others, and what is the expected age of the audience for these movies?
 
+WITH Top5Movies AS (
+    SELECT
+        f.film_id,
+        f.title,
+        f.rating,
+        COUNT(r.rental_id) AS rental_count
+    FROM
+        film f
+    JOIN rental r ON f.film_id = r.inventory_id
+    GROUP BY
+        f.film_id, f.title, f.rating
+    ORDER BY
+        rental_count DESC
+    LIMIT 5
+)
+
+SELECT
+    t.film_id,
+    t.title,
+    t.rating,
+    t.rental_count
+FROM
+    Top5Movies t
+ORDER BY
+    t.rental_count DESC;
+
 --Which actors/actresses didn't act for a longer period of time than the others?
 
